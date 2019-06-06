f35a679c03828d138f488ef2ca9533aa437be838
saru <sarumathiayyappan@gmail.com>
2019-06-06 11:34:07 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,8 @@
+v1=int(input("enter the value1  "))
+v2=int(input("enter the value2  "))
+mul=v1*v2
+print(mul)
+if(mul%2==0):
+  print("the multiply value is even")
+else:
+  print("the multiply value is odd")
